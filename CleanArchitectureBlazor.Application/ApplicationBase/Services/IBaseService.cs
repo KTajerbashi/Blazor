87db09937b39eb3e98f13b9cac756a2a8b6745c8
Tajerbashi <kamrantajerbashi@gmail.com>
2024-09-09 19:59:20 +0330

    Feature : Add Application Repository

---
+++ CleanArchitectureBlazor.Application/ApplicationBase/Services/IBaseService.cs
@@ -0,0 +1,22 @@
+﻿namespace CleanArchitectureBlazor.Application.ApplicationBase.Services;
+
+public interface IBaseService<TEntity>
+{
+    Task<TEntity> GetAsync(Guid key);
+    TEntity Get(Guid key);
+
+    Task<TEntity> GetAsync(long id);
+    TEntity Get(long id);
+
+    Task<IEnumerable<TEntity>> GetAsync();
+    IEnumerable<TEntity> Get();
+
+    Guid Insert(TEntity entity);
+    Guid InsertAsync(TEntity entity);
+
+    void Delete(Guid key);
+    Task DeleteAsync(Guid key);
+
+}
+
+
---
+++ CleanArchitectureBlazor.Application/Repositories/Security/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using CleanArchitectureBlazor.Application.ApplicationBase.Services;
+using CleanArchitectureBlazor.Core.Domains.Security;
+
+namespace CleanArchitectureBlazor.Application.Repositories.Security;
+
+public interface IUserService : IBaseService<UserEntity>
+{
+}
