99fd7c702e8a0b036ebf125bdf55fbdbb728d12c
KTajerbashi <kamrantajerbashi@gmail.com>
2024-09-10 21:44:09 +0330

    Feature : Add Some Entities

---
+++ CleanArchitectureBlazor.Core/Domains/Privilege/RoleMenuEntity.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitectureBlazor.Core.CoreBase.Entities;
+using CleanArchitectureBlazor.Core.Domains.Security;
+using CleanArchitectureBlazor.Core.Domains.Setting;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArchitectureBlazor.Core.Domains.Privilege;
+
+[Table("RoleMenu", Schema = "Privilege"), Description("دسترسی منو بر اساس نقش")]
+public class RoleMenuEntity : Entity 
+{
+    [ForeignKey(nameof(Menu))]
+    public long MenuId { get; set; }
+    public virtual MenuEntity Menu { get; set; }
+
+    [ForeignKey(nameof(Role))]
+    public long RoleId { get; set; }
+    public virtual RoleEntity Role { get; set; }
+}
--- CleanArchitectureBlazor.Core/Domains/Security/RoleEntity.cs
+++ CleanArchitectureBlazor.Core/Domains/Security/RoleEntity.cs
@@ -1,4 +1,5 @@
 ﻿using CleanArchitectureBlazor.Core.CoreBase.Entities;
+using CleanArchitectureBlazor.Core.Domains.Privilege;
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -6,4 +7,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace CleanArchitectureBlazor.Core.Domains.Security;
 
 [Table("Role", Schema = "Security"), Description("نقش")]
-public class RoleEntity : IdentityRole<long>, IEntity {}
+public class RoleEntity : IdentityRole<long>, IEntity
+{
+    public virtual ICollection<RoleMenuEntity> RoleMenuEntities { get; set; }
+
+}
