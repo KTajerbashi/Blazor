f2442382b154ea21d272ed1f730845146544b90d
KTajerbashi <kamrantajerbashi@gmail.com>
2024-09-17 01:23:50 +0330

    Update DI ApplicationService

--- Source/1.Core/CleanArchitectureBlazor.Core.ApplicationService/Extensions/ApplicationExtensions.cs
+++ Source/1.Core/CleanArchitectureBlazor.Core.ApplicationService/Extensions/ApplicationExtensions.cs
@@ -1,5 +1,11 @@
-﻿namespace CleanArchitectureBlazor.Core.ApplicationService.Extensions;
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitectureBlazor.Core.ApplicationService.Extensions;
 
 public static class ApplicationExtensions
 {
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        return services;
+    }
 }
