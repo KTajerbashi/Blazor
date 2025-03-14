72247639bc48f1581e1d5a6d3671634edc225595
KTajerbashi <kamrantajerbashi@gmail.com>
2025-03-14 17:22:18 +0330

    Feature : Blazor Server Presentation Layer Completed

---
+++ Src/03.Presentation/CleanArchitectureBlazor.WebApp/DependencyInjections.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitectureBlazor.Core.Application;
+using CleanArchitectureBlazor.Infra.Data.SqlServer;
+using CleanArchitectureBlazor.Infra.Data.SqlServer.Common.DataBase;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitectureBlazor.WebApp;
+
+public static class DependencyInjections
+{
+    public static IServiceCollection AddWebAppDependecies(this IServiceCollection services, IConfiguration configuration)
+        => services
+        .AddInfraDataDependecies(configuration)
+        .AddApplicationDependecies()
+        ;
+  
+}
--- Src/03.Presentation/CleanArchitectureBlazor.WebApp/Program.cs
+++ Src/03.Presentation/CleanArchitectureBlazor.WebApp/Program.cs
@@ -1,11 +1,35 @@
 using CleanArchitectureBlazor.WebApp;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using CleanArchitectureBlazor.WebApp.Data;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+var builder = WebApplication.CreateBuilder(args);
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+IConfiguration configuration = builder.Configuration;
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+builder.Services.AddWebAppDependecies(configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
