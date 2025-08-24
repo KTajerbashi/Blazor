d96744deb2257eabe6d2f344f78f5e3709a3b94f
KTajerbashi <kamrantajerbashi@gmail.com>
2025-08-24 21:24:00 +0330

    #Fix Handlers and Middleware

---
+++ Src/03.Presentation/BlazorSolution.WebApp/Middlewares/ExceptionHandler/ApiExceptionResponse.cs
@@ -0,0 +1,34 @@
+﻿namespace BlazorSolution.WebApp.Middlewares.ExceptionHandler;
+
+// API Response Models
+public class ApiExceptionResponse
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public string ErrorMessage { get; set; }
+    public string? Details { get; set; }
+
+    public ApiExceptionResponse(int statusCode, string message, string? details = null)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        Details = details;
+    }
+
+    public ApiExceptionResponse(int statusCode)
+    {
+        StatusCode = statusCode;
+        Message = GetDefaultMessage(statusCode);
+    }
+
+    private static string GetDefaultMessage(int statusCode) =>
+        statusCode switch
+        {
+            400 => "Bad Request",
+            401 => "Unauthorized",
+            404 => "Resource Not Found",
+            422 => "Validation Error",
+            500 => "Internal Server Error",
+            _ => "An error occurred"
+        };
+}
---
+++ Src/03.Presentation/BlazorSolution.WebApp/Middlewares/ExceptionHandler/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorSolution.WebApp.Middlewares.ExceptionHandler;
+
+public static class ExceptionMiddlewareExtensions
+{
+    public static IApplicationBuilder UseApiExceptionHandler(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ApiExceptionMiddleware>();
+    }
+}
